b1c9a875d5ef4dc2847328dc7e16d4fbea511404
Dzejkoubi <kubakraus06@gmail.com>
2025-01-05 22:24:40 +0100

    Refactor LikedRecepiesScreen to display messages when no recipes are liked; add navigation buttons to find or random recipes; remove unused test meal data

--- lib/screens/liked_recepies_screen/liked_recepies_screen.dart
+++ lib/screens/liked_recepies_screen/liked_recepies_screen.dart
@@ -1,8 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:receptar/app/const/style_constants.dart';
+import 'package:receptar/app/router/router.dart';
 import 'package:receptar/app/shared/styled/styled_text.dart';
 import 'package:receptar/app/shared/widgets/bottom_navigation_bar.dart';
+import 'package:receptar/app/shared/widgets/helper_widgets.dart';
+import 'package:receptar/app/shared/widgets/styled_button.dart';
 import 'package:receptar/models/recepe_model.dart';
 import 'package:receptar/models/test_mode.dart';
 import 'package:receptar/screens/show_recepie_screens/show_recepie_expandable_tab.dart';
@@ -16,9 +19,6 @@ class LikedRecepiesScreen extends StatefulWidget {
 }
 
 class _LikedRecepiesScreenState extends State<LikedRecepiesScreen> {
-  final Meal testMeal = MealTest.testMeal();
-  final Map testMealMap = MealTest.testMeal().toMap();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,22 +29,36 @@ class _LikedRecepiesScreenState extends State<LikedRecepiesScreen> {
       ),
       body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShowRecepieExpandableTab(
-                id: testMeal.id,
-                name: testMeal.name,
-                category: testMeal.category,
-                area: testMeal.area,
-                steps: testMeal.steps,
-                thumbPhoto: testMeal.thumbPhoto,
-                tags: testMeal.tags,
-                youtubeLink: testMeal.youtubeLink,
-                ingredients: testMeal.ingredients,
-                measures: testMeal.measures,
-              ),
-            ],
+          child: Center(
+            child: Column(
+              children: [
+                VerticalSpace(height: 16),
+                const StyledBodyTextImportant(
+                  text: "You have not liked any recepies yet.",
+                ),
+                VerticalSpace(height: 16),
+                const StyledBodyTextImportant(
+                  text:
+                      "Go to the 'Find a recipe' or 'Random recipe' screen to find a recipe you like.",
+                ),
+                VerticalSpace(height: 16),
+                StyledButton(
+                  text: "Find a recipe",
+                  icon: Icons.search,
+                  onPressed: () {
+                    context.router.push(const FindRecepieRoute());
+                  },
+                ),
+                const SizedBox(height: 20),
+                StyledButton(
+                  text: "Random recipe",
+                  icon: Icons.shuffle,
+                  onPressed: () {
+                    context.router.push(const RandomRecepieRoute());
+                  },
+                ),
+              ],
+            ),
           )),
       floatingActionButton: FloatingActionButton.small(
         onPressed: () {},
@@ -53,3 +67,6 @@ class _LikedRecepiesScreenState extends State<LikedRecepiesScreen> {
     );
   }
 }
+
+
+//TODO: solve the favourite recipes feature
